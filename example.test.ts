31229215742b13a96684ee58f21e7c5272f78a44
Thiago Rosa <thiagosrosa@gmail.com>
2024-08-23 11:51:01 -0300

    test: create API tests using Supertest

--- example.test.ts
+++
@@ -1,3 +0,0 @@
-test("soma de 1 + 1 deve ser 2", () => {
-  expect(1 + 1).toBe(2);
-});
---
+++ src/main.test.ts
@@ -0,0 +1,51 @@
+import { app } from "./main";
+import request from "supertest";
+
+describe("Orders API", () => {
+  it("should create a new order", async () => {
+    const newOrder = { items: ["item1", "item2"] };
+
+    const response = await request(app).post("/pedidos").send(newOrder);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.items).toEqual(newOrder.items);
+    expect(response.body.status).toBe("pendente");
+  });
+
+  it("should return all orders", async () => {
+    const newOrder = { items: ["item1", "item2"] };
+    await request(app).post("/pedidos").send(newOrder);
+
+    const response = await request(app).get("/pedidos");
+
+    expect(response.status).toBe(200);
+
+    console.log(response.body);
+
+    expect(response.body.length).toBe(2);
+    expect(response.body[0].items).toEqual(newOrder.items);
+  });
+
+  it("should update the status of an existing order", async () => {
+    const newOrder = { items: ["item1", "item2"] };
+    const createResponse = await request(app).post("/pedidos").send(newOrder);
+
+    const orderId = createResponse.body.id;
+    const response = await request(app)
+      .put(`/pedidos/${orderId}/status`)
+      .send({ status: "completed" });
+
+    expect(response.status).toBe(200);
+    expect(response.body.status).toBe("completed");
+  });
+
+  it("should return 404 when trying to update the status of a non-existent order", async () => {
+    const response = await request(app)
+      .put(`/pedidos/999/status`)
+      .send({ status: "completed" });
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty("message", "Pedido não encontrado");
+  });
+});
