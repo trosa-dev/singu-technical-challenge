731c6db6c945a65b716b35e40f1954335553a961
Thiago Rosa <thiagosrosa@gmail.com>
2024-08-23 11:34:20 -0300

    chore: update Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
+    "^.+\\.tsx?$": ["ts-jest", {}],
   },
   testPathIgnorePatterns: ["/dist/"],
+
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+    "./src/main.ts": {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 };
