66734ef25901b829e10722a19f746619f572d38e
Thiago Rosa <thiagosrosa@gmail.com>
2024-08-25 00:43:04 -0300

    refactor: update router to utilize middleware

--- src/infra/http/routes/order.routes.ts
+++ src/infra/http/routes/order.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { ordersController } from "../controllers/orders.controller";
+import { OrdersController } from "../controllers/orders.controller";
 
 export const ordersRouter = Router();
 
-ordersRouter.post("/", (req, res) => ordersController.createOrder(req, res));
-ordersRouter.get("/", (req, res) => ordersController.getAllOrders(req, res));
-ordersRouter.put("/:id/status", (req, res) =>
-  ordersController.updateOrderStatus(req, res)
-);
+const ordersController = new OrdersController();
+
+ordersRouter.post("/", ordersController.createOrder);
+ordersRouter.get("/", ordersController.getAllOrders);
+ordersRouter.put("/:id/status", ordersController.updateOrderStatus);
 
 export default ordersRouter;
