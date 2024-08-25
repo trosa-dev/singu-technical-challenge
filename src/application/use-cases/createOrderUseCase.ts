011a5a62b01e7c8e9b5df9bf8d158d785c5fdd76
Thiago Rosa <thiagosrosa@gmail.com>
2024-08-25 00:29:00 -0300

    feat: add createOrderUseCase

---
+++ src/application/use-cases/createOrderUseCase.ts
@@ -0,0 +1,20 @@
+import { HttpStatus } from "./../../shared/enums/httpStatus";
+import { Order, OrderItem } from "../../domain/entities/order/order";
+import { OrderRepository } from "../../domain/repositories/orderRepository";
+import { AppError } from "../errors/appError";
+
+export class CreateOrderUseCase {
+  constructor(private orderRepository: OrderRepository) {}
+
+  async execute(items: OrderItem[]): Promise<Order> {
+    if (!items || items.length === 0) {
+      throw new AppError(
+        "An order must have at least 1 item",
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    const order = new Order(items, Date.now().toString());
+    return this.orderRepository.create(order);
+  }
+}
