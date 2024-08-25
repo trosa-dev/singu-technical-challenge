00829a3424562b2889543fd84e6bd90986bcfa14
Thiago Rosa <thiagosrosa@gmail.com>
2024-08-25 00:42:11 -0300

    refactor: restructure OrdersController for Clean Architecture compliance

--- src/infra/http/controllers/orders.controller.ts
+++ src/infra/http/controllers/orders.controller.ts
@@ -1,35 +1,44 @@
-import { Request, Response } from "express";
-import { orders } from "../../config/expressApp";
-import { OrderStatus } from "../../../domain/entities/order/enums/order-status.enum";
+import { GetAllOrdersUseCase } from "./../../../application/use-cases/getAllOrders.useCase";
+import { NextFunction, Request, Response } from "express";
 import HttpStatus from "../../../shared/enums/httpStatus";
+import { InMemoryOrderRepository } from "../../database/inMemoryOrderRepository";
+import { CreateOrderUseCase } from "../../../application/use-cases/createOrderUseCase";
+import { UpdateOrderStatusUseCase } from "../../../application/use-cases/updateOrderStatus.useCase";
+import { AppError } from "../../../application/errors/appError";
 
-class OrdersController {
-  createOrder(req: Request, res: Response): Response {
-    const order = {
-      id: Date.now().toString(),
-      items: req.body.items,
-      status: OrderStatus.PENDING,
-    };
-    orders.push(order);
+const orderRepository = new InMemoryOrderRepository();
+
+const createOrderUseCase = new CreateOrderUseCase(orderRepository);
+const getAllOrdersUseCase = new GetAllOrdersUseCase(orderRepository);
+const updateOrderStatusUseCase = new UpdateOrderStatusUseCase(orderRepository);
+
+export class OrdersController {
+  async createOrder(req: Request, res: Response): Promise<Response> {
+    const order = await createOrderUseCase.execute(req.body.items);
     return res.status(HttpStatus.CREATED).send(order);
   }
 
-  getAllOrders(req: Request, res: Response): Response {
+  async getAllOrders(req: Request, res: Response): Promise<Response> {
+    const orders = await getAllOrdersUseCase.execute();
     return res.status(HttpStatus.OK).send(orders);
   }
 
-  updateOrderStatus(req: Request, res: Response): Response {
-    const pedido = orders.find((p: any) => p.id === req.params.id);
-    if (pedido) {
-      pedido.status = req.body.status;
-      return res.status(HttpStatus.OK).send(pedido);
-    } else {
-      return res
-        .status(HttpStatus.NOT_FOUND)
-        .send({ message: "Pedido não encontrado" });
+  async updateOrderStatus(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id: string = req.params.id;
+      const newStatus: string = req.body.newStatus;
+
+      const updatedOrder = await updateOrderStatusUseCase.execute(
+        id,
+        newStatus
+      );
+      return res.status(HttpStatus.OK).send(updatedOrder);
+    } catch (error: any) {
+      if (error instanceof AppError) {
+        next(error);
+      } else {
+        res.status(500).json({ message: "Internal Server Error" });
+      }
     }
   }
 }
-
-const ordersController = new OrdersController();
-export { ordersController };
