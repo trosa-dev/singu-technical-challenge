34e702a242e00b246a8dccc1ed7c3d7f102f7952
Thiago Rosa <thiagosrosa@gmail.com>
2024-08-25 23:23:06 -0300

    refactor: add INTERNAL_SERVER_ERROR to responseMessages

--- src/constants/messages/responseMessages.ts
+++ src/constants/messages/responseMessages.ts
@@ -8,5 +8,6 @@ export const responseMessages = {
     INVALID_ORDER_STATUS: "Invalid order status",
     ID_ALREADY_EXISTS: "Order ID already exists",
     NO_ITEMS_IN_ORDER: "An order must have at least 1 item",
+    INTERNAL_SERVER_ERROR: "Internal server Error",
   },
 };
